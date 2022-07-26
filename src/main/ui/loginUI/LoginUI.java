e0feff0ba15d75c7fc11a95805b2bdaf9e425c74
Hugo2020-git <hghg308@gmail.com>
2022-07-26 15:14:40 -0400

    Se actualiza UI login

--- src/main/ui/loginUI/LoginUI.java
+++ src/main/ui/loginUI/LoginUI.java
@@ -12,5 +12,17 @@ public class LoginUI {
 	public static By icoProforma = By.className("ico_prof");
 	//---------------- alert error ------------
 	public static By buttonAcceptAlertError = By.className("botones-modal");
+	//------------------ nuevo ----------------------------
+	public static By ID_CAMPO_USUARIO                  = By.id("login");
+	public static By ID_CAMPO_CONTRASENIA              = By.id("password");
+	public static By ID_BOTON_INGRESAR                 = By.id("btn-ingresar");
+	public static By ID_VENTANA_MODAL                  = By.id("modal_window");
+	public static By ID_MENSAJE_MODAL                  = By.id("mensaje-modal");
+	public static By PATH_BOTON_VENTANA_MODAL          = By.xpath("//*[@id='modal_window']/div[2]/div[2]/button");
+	public static By ID_CAMPO_BUSQUEDA_MODULO          = By.id("txt-buscar-modulo");
+	public static By ID_CAMPO_CONTRASENIA_ACTUAL       = By.id("password-actual");
+	public static By ID_CAMPO_CONTRASENIA_NUEVA        = By.id("password-nuevo");
+	public static By ID_CAMPO_CONTRASENIA_CONFIRMACION = By.id("password-confirmacion");
+	public static By ID_BOTON_GRABAR                   = By.id("btn-grabar");
 
 }
